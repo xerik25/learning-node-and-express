fb03a2634908cca9c589df5d50523e5623397a37
Landonius Rex: King of the Landons <landon.cline@selectquotesenior.com>
2016-10-25 16:59:00 -0500

    Chapter Templating Engines: HandleBars

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.set('views','./src/views');
-app.set('view engine','jade');
+
+var handlebars = require('express-handlebars');
+app.engine('.hbs', handlebars({extname: '.hbs'}));
+
+app.set('view engine','.hbs');
 
 app.get('/', function(req, res) {
-  res.render('index', {list: ['a', 'b']});
+  res.render('index', {title: 'Hello from render', list: ['a', 'b']});
 });
 
 app.get('/books', function(req, res) {
