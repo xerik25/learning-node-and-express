3a844e1db68298fce04c437239f3588ea4a57f77
Landonius Rex: King of the Landons <landon.cline@selectquotesenior.com>
2016-10-25 14:32:16 -0500

    Finished chapter: Setting up Gulp

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ var express = require('express');
 
 var app = express();
 
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
-app.get('/', function(req, res){
-	res.send('hello world');
+app.get('/', function(req, res) {
+  res.send('hello world');
 });
 
-app.get('/books', function(req, res){
-	res.send('hello books');
+app.get('/books', function(req, res) {
+  res.send('hello books');
 });
 
-app.listen(port, function(err){
-	console.log('running server on port ' + port)
+app.listen(port, function(err) {
+  console.log('running server on port ' + port);
 });
