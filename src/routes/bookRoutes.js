f330299588096872fa3e33d98c6576f21e2533b8
Landonius Rex: King of the Landons <landon.cline@selectquotesenior.com>
2016-10-27 23:57:16 -0500

    Chapter Structure/APIs: finished controllers

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -5,43 +5,14 @@ var mongodb = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectID;
 
 var router = function(nav) {
-  bookRouter.use(function (req, res, next) {
-    if (!req.user) {
-      res.redirect('/');
-    }
-    next();
-  });
+  var bookController = require('../controllers/bookController')(null, nav);
+  bookRouter.use(bookController.middleware);
 
   bookRouter.route('/')
-    .get(function(req, res) {
-      var url = 'mongodb://localhost:27017/libraryApp';
-      mongodb.connect(url, function (err, db) {
-        var collection = db.collection('books');
-        collection.find({}).toArray(function (err, results) {
-          res.render('bookListView',{
-            title: 'Books',
-            nav: nav,
-            books: results
-          });
-        });
-      });
-    });
+    .get(bookController.getIndex);
 
   bookRouter.route('/:id')
-    .get(function(req, res) {
-      var id = new ObjectId(req.params.id);
-      var url = 'mongodb://localhost:27017/libraryApp';
-      mongodb.connect(url, function (err, db) {
-        var collection = db.collection('books');
-        collection.findOne({_id: id}, function (err, results) {
-          res.render('bookView',{
-            title: 'Books',
-            nav: nav,
-            book: results
-          });
-        });
-      });
-    });
+    .get(bookController.getById);
 
   return bookRouter;
 };
